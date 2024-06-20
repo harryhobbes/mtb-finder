22ef98152b21f0cd1f98b64c5a36d5da3a0b3f81
harryhobbes <harryhobbes@gmail.com>
2024-06-20 13:38:02 +1000

    Added new migrations for more advanced notifications, user settings etc.

--- app/migration.sql
+++ app/migration.sql
@@ -1,2 +1,27 @@
 -- ALTER TABLE deal 
 -- ADD COLUMN lowest_deal_text text;
+
+CREATE TABLE website (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  base_url TEXT NOT NULL,
+  css_selector TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+ALTER TABLE deal
+ADD COLUMN website_id INTEGER NOT NULL DEFAULT '';
+ADD COLUMN access INTEGER NOT NULL DEFAULT '';
+
+ALTER TABLE user
+ADD COLUMN slack_api_key TEXT;
+
+CREATE TABLE deal_comms (
+    deal_id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
+    comms_type TEXT NOT NULL DEFAULT 'ALL',
+    FOREIGN KEY (deal_id) REFERENCES deal (id),
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
